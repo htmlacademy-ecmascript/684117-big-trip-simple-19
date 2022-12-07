5b669e4bc3b0c41fc0e6cc2f6321ff2b867353c4
EgorBaydashev <baidashevegor@yandex.ru>
2022-12-08 01:30:21 +0500

    Правка модуля добавления новой точки маршрута

--- src/view/point-add-view.js
+++ src/view/point-add-view.js
@@ -2,7 +2,8 @@ import {createElement} from '../render.js';
 
 function createPointAddTemplate() {
   return (
-    `<form class="event event--edit" action="#" method="post">
+    `<li class="trip-events__item">
+    <form class="event event--edit" action="#" method="post">
     <header class="event__header">
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
@@ -161,7 +162,8 @@ function createPointAddTemplate() {
         </div>
       </section>
     </section>
-  </form>`
+    </form>
+    </li>`
   );
 }
 
