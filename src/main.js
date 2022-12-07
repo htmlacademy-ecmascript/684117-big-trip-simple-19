62f94d2d62c5a882747202805e335b633251b193
EgorBaydashev <baidashevegor@yandex.ru>
2022-12-07 23:57:26 +0500

    Правки. Удалит модуль Кнопка нового события

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import FilterView from "./view/filter-view.js";
-import NewEventButtonView from "./view/new-event-button-view.js";
 import {render} from "./render.js";
 
 const siteHeader = document.querySelector('.page-header');
 const siteFiltersElement = siteHeader.querySelector('.trip-controls__filters');
-const siteHeaderElement = siteHeader.querySelector('.trip-main');
 
 render(new FilterView(), siteFiltersElement);
-render(new NewEventButtonView(), siteHeaderElement);
