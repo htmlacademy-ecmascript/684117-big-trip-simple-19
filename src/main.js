92aa979fe6be6972261eba50632cc307c37444e8
EgorBaydashev <baidashevegor@yandex.ru>
2022-12-07 23:53:30 +0500

    Добавит модуль Фильтры

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import NewEventButtonView from "./view/new-event-button-view";
+import FilterView from "./view/filter-view.js";
+import NewEventButtonView from "./view/new-event-button-view.js";
 import {render} from "./render.js";
 
 const siteHeader = document.querySelector('.page-header');
+const siteFiltersElement = siteHeader.querySelector('.trip-controls__filters');
 const siteHeaderElement = siteHeader.querySelector('.trip-main');
 
+render(new FilterView(), siteFiltersElement);
 render(new NewEventButtonView(), siteHeaderElement);
