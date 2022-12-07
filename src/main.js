fcb66537cd1a2a83f2b30d1b873dc6838c4b80ae
EgorBaydashev <baidashevegor@yandex.ru>
2022-12-08 00:13:30 +0500

    Добавит модуль Сортировка

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import FilterView from "./view/filter-view.js";
+import SortView from "./view/sort-view.js";
 import {render} from "./render.js";
 
 const siteHeader = document.querySelector('.page-header');
 const siteFiltersElement = siteHeader.querySelector('.trip-controls__filters');
+const siteMain = document.querySelector('.page-body__page-main');
+const siteMainSection = siteMain.querySelector('.trip-events');
 
 render(new FilterView(), siteFiltersElement);
+render(new SortView(), siteMainSection);
