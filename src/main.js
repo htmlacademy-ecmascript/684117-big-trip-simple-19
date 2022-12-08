bbfcb7fd2b3a893f9908198329be539af5761432
EgorBaydashev <baidashevegor@yandex.ru>
2022-12-08 11:56:25 +0500

    Добавит презентер списка

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 import FilterView from "./view/filter-view.js";
 import SortView from "./view/sort-view.js";
-//import PointListView from "./view/point-list-view.js";
-//import PointAddView from "./view/point-add-view.js";
-//import PointEditView from "./view/point-edit-view.js";
-//import PointView from "./view/point-view.js";
 import {render} from "./render.js";
+import ListPresenter from "./presenter/list-presenter.js";
 
 const siteHeader = document.querySelector('.page-header');
 const siteFiltersElement = siteHeader.querySelector('.trip-controls__filters');
 const siteMain = document.querySelector('.page-body__page-main');
 const siteMainSection = siteMain.querySelector('.trip-events');
+const listPresenter = new ListPresenter({listContainer: siteMainSection});
 
 render(new FilterView(), siteFiltersElement);
 render(new SortView(), siteMainSection);
-//render(new PointListView(), siteMainSection);
-//render(new PointAddView(), siteMainSection);
-//render(new PointEditView(), siteMainSection);
-//render(new PointView(), siteMainSection);
+
+listPresenter.init();
